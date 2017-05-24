f43945a261dafd698dc36be85eb3fbdf787d7af4
Conor Mc Keever <conormckeever92@gmail.com>
2017-05-24 15:08:02 +0100

    updated paul_trap.py

--- paul_trap.py
+++ paul_trap.py
@@ -10,6 +10,10 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
 from scipy.optimize import minimize
+from qutip import *
+import time
+from mpl_toolkits.mplot3d import Axes3D
+
 
 
 class Harmonic_Paul_trap(object):
@@ -19,12 +23,6 @@ class Harmonic_Paul_trap(object):
 		self.V = V # volts
 		self.r = r # metres
 
-	def r(self):
-		return np.array(self.r)
-
-	def V(self):
-		return np.array(self.V)
-		
 	def field(self, x):
 		# assume trap is centred at x = 0
 		#*codata.value('atomic unit of length')
@@ -56,31 +54,40 @@ class Nanosphere(object):
 		self.sphere_radius = sphere_radius
 		self.sphere_mass = sphere_mass
 
-		
+
+
+	def radius(self):
+		#radius in m
+		return np.array(self.sphere_radius)
+
+
+
 	def charge(self):
-		
+		#charge in units of electron charge
 		charge_val = codata.value('elementary charge')*self.sphere_charge
 		return np.array(charge_val)
 	
+
+
 	def mass(self):
-		
-		#mass_val = codata.value('atomic mass constant')*self.sphere_mass
+		#Mass in kG
 		return np.array(self.sphere_mass)
 
 
+
 	def field(self,x,x_sphere):
-		
-		#x_atomic = x*codata.value('atomic unit of length')
-		#x_sphere_atomic = x_sphere*codata.value('atomic unit of length')
+		#use SI unit of vacuum permitivity
+		#x input in metres
 		vac_perm = codata.value('electric constant')
 		field_val = self.charge()/(4.0*np.pi*vac_perm)/np.absolute(x-x_sphere)
 		
 		return np.array(field_val)
 	
+
+
 	def dxfield(self,x1,x2):
-		
-		#x1_units = x1*codata.value('atomic unit of length')
-		#x2_units = x2*codata.value('atomic unit of length')
+		#use SI unit of vacuum permitivity
+		#x input in metres
 		vac_perm = codata.value('electric constant')
 		field_val = -self.charge()/(4.0*np.pi*vac_perm)/np.power(x1-x2,2)
 
@@ -89,10 +96,11 @@ class Nanosphere(object):
 		elif x2 > x1:
 			return np.array(-field_val)
 
+
+
 	def d2xfield(self,x1,x2):
-		
-		#x1_units = x1*codata.value('atomic unit of length')
-		#x2_units = x2*codata.value('atomic unit of length')
+		#use SI unit of vacuum permitivity
+		#x input in metres
 		vac_perm = codata.value('electric constant')
 		field_val = 2 * self.charge()/(4.0*np.pi*vac_perm)/np.absolute(np.power(x1-x2,3))
 		
@@ -104,8 +112,8 @@ class Nanosphere(object):
 	def dydxfield(self,x1,x2):
 		
 
-		#x1_units = x1*codata.value('atomic unit of length')
-		#x2_units = x2*codata.value('atomic unit of length')
+		#use SI unit of vacuum permitivity
+		#x input in metres
 		vac_perm = codata.value('electric constant')
 		field_val = -self.charge()/(4.0*np.pi*vac_perm)/np.absolute(np.power(x1-x2,3))
 		
@@ -116,67 +124,10 @@ class Nanosphere(object):
 
 
 
-class Potential(object):
-	def __init__(self, trap1, sphere_1, sphere_2):
-		
-		self.trap1 = trap1
-		self.sphere_1 = sphere_1
-		self.sphere_2 = sphere_2
-
-
-	def omegaT(self):
-
-		#wT1 = 2 * self.sphere_1.sphere_charge * self.trap1.V / ( self.sphere_1.sphere_mass * self.trap1.r * self.trap1.r )
-		#wT2 = 2 * self.sphere_2.sphere_charge * self.trap1.V / ( self.sphere_2.sphere_mass * self.trap1.r * self.trap1.r )
-
-		wT1 = 2 * self.sphere_1.charge() * self.trap1.V / ( self.sphere_1.mass() * self.trap1.r * self.trap1.r )
-		wT2 = 2 * self.sphere_2.charge() * self.trap1.V / ( self.sphere_2.mass() * self.trap1.r * self.trap1.r )
-
-		return np.sqrt( np.absolute( np.array([wT1,wT2]) ) )
-
-	def delta_z(self):
-		hbar = codata.value('Planck constant')
-		dZ1 = np.sqrt(  np.absolute(  hbar/(  2* self.sphere_1.sphere_mass* self.omegaT()[[0]]  )   )   )
-		dZ2 = np.sqrt(  np.absolute(  hbar/(  2* self.sphere_2.sphere_mass* self.omegaT()[[1]]  )   )   )
-		return np.array([dZ1,dZ2])
-		
-	def potential(self,x):        
-		
-		potential_val_trap = self.sphere_1.charge()*self.trap1.field(x[[0]]) + self.sphere_2.charge()*self.trap1.field(x[[1]])
-		potential_val_ions = self.sphere_1.charge()*self.sphere_2.field(x[[0]],x[[1]]) + self.sphere_2.charge()*self.sphere_1.field(x[[0]],x[[1]])
-		potential_val = potential_val_trap + potential_val_ions
-		
-		return np.array(potential_val)
-	
-	def equilibrium(self):
-		x0 = np.array([-1,1])
-		res = minimize(self.potential, x0, method='nelder-mead',options={'xtol': 1e-8, 'disp': True})
-		
-		return res.x
-
-	def hessian(self, x):
-
-		A = np.zeros([2,2])
-
-		A[0,0] = self.sphere_1.charge()*self.trap1.d2xfield(x[[0]]) + self.sphere_1.charge()*self.sphere_2.d2xfield(x[[0]],x[[1]]) + self.sphere_2.charge()*self.sphere_1.d2xfield(x[[0]],x[[1]])
-		A[1,1] = self.sphere_1.charge()*self.trap1.d2xfield(x[[1]]) + self.sphere_1.charge()*self.sphere_2.d2xfield(x[[1]],x[[0]]) + self.sphere_2.charge()*self.sphere_1.d2xfield(x[[1]],x[[0]])
-		A[0,1] = self.sphere_1.charge()*self.sphere_2.dydxfield(x[[0]],x[[1]]) + self.sphere_2.charge()*self.sphere_1.dydxfield(x[[0]],x[[1]])
-		A[1,0] = self.sphere_1.charge()*self.sphere_2.dydxfield(x[[1]],x[[0]]) + self.sphere_2.charge()*self.sphere_1.dydxfield(x[[1]],x[[0]])
-		#change outputs to matrices??
-
-		return A
-
-	def S_matrix(self):
-
-		A = self.hessian( self.equilibrium() )
-		eig_vals = np.linalg.eig(A)[0]
-		S = np.linalg.eig(A)[1] 
-
-		return S
-
-
 
 class B_field(object):
+
+	#this code is specific to a linar field gradient
 
 	def __init__(self,field_grad):
 
@@ -185,73 +136,133 @@ class B_field(object):
 
 	def omega(self, x, n=1):
 
-		#field_grad_units = self.field_grad * codata.value('atomic unit of electric field gradient')
 		ge = codata.value('electron g factor')
 		muB = codata.value('Bohr magneton')
 		omega = ge*muB*self.field_grad*x[[n]]
-
 		return omega
 	
-	def dxomega(self, x, n=1):
+	def dxomega(self):
+
 
 		hbar = codata.value("Planck constant")
 		ge = codata.value('electron g factor')
 		muB = codata.value('Bohr magneton')
-		
 		omega = ge*muB/hbar*self.field_grad
-
 		return omega
 
 
 
-sphere_1 = Nanosphere(1.0, 1e-8, 281*1.67e-27)
-sphere_2 = Nanosphere(1.0, 1e-8, 171*1.67e-27)
-trap_1 = Harmonic_Paul_trap(-1000.0, 0.1)
-potential_1 = Potential(trap_1,sphere_1,sphere_2)
-B_field = B_field(10)
+class System(object):
+	def __init__(self, trap1, sphere_1, sphere_2, B_field):
+		
+		self.trap1 = trap1
+		self.sphere_1 = sphere_1
+		self.sphere_2 = sphere_2
+		self.B_field = B_field
 
 
-omegaT_vec = np.transpose( [np.array( potential_1.omegaT()) ])
-dxomega = B_field.dxomega(1)
-dxomega_vec = np.array([[dxomega],[dxomega]])
-deltaZ = potential_1.delta_z()
+	def omegaT(self):
 
-dxomega_mat = np.concatenate( (dxomega_vec, dxomega_vec), axis=1)
-deltaZ_mat = np.concatenate( (np.transpose(deltaZ), np.transpose(deltaZ)), axis=0)
-omegaT_mat = np.concatenate( (np.transpose(omegaT_vec), np.transpose(omegaT_vec)), axis=0)
-S_matrix = potential_1.S_matrix()
+		#calculate the energy frequncy spacing of harmonic oscillator
+		wT1 = 2 * self.sphere_1.charge() * np.array(self.trap1.V) / ( self.sphere_1.mass() * np.array( self.trap1.r ) * np.array( self.trap1.r ))
+		wT2 = 2 * self.sphere_2.charge() * np.array(self.trap1.V) / ( self.sphere_2.mass() * np.array( self.trap1.r ) * np.array( self.trap1.r ))
+		return np.sqrt( np.absolute( np.array([wT1,wT2]) ) )
 
 
-print("Smatrix")
-print(S_matrix)
+	def delta_z(self):
+		hbar = codata.value('Planck constant')
+		dZ1 = np.sqrt(  np.absolute(  hbar/(  2* self.sphere_1.sphere_mass* self.omegaT()[[0]]  )   )   )
+		dZ2 = np.sqrt(  np.absolute(  hbar/(  2* self.sphere_2.sphere_mass* self.omegaT()[[1]]  )   )   )
+		return np.array([dZ1,dZ2])
+		
 
-print("deltaZ")
-print(  deltaZ )
+	def potential(self,x):        
+		
+		potential_val_trap = self.sphere_1.charge()*self.trap1.field(x[[0]]) + self.sphere_2.charge()*self.trap1.field(x[[1]])
+		potential_val_ions = self.sphere_1.charge()*self.sphere_2.field(x[[0]],x[[1]]) + self.sphere_2.charge()*self.sphere_1.field(x[[0]],x[[1]])
+		potential_val = potential_val_trap + potential_val_ions
+		return np.array(potential_val)
+	
 
-print("deltaZ Matrix")
-print(  deltaZ_mat )
+	def equilibrium(self):
 
-print("dxomega_vec")
-print(dxomega_vec)
+		x0 = np.array([-1,1])
 
-print("dxomega_Matrix")
-print(dxomega_mat)
-
-print("omegaT_vec")
-print(omegaT_vec )
-
-print("omegaT_mat")
-print(omegaT_mat )
-
-epsilon = S_matrix*deltaZ_mat*dxomega_mat/omegaT_mat
-
-couplings = np.asmatrix( np.transpose(epsilon) ) * np.asmatrix( epsilon*np.transpose(omegaT_mat))
-
-print("epsilon")
-print(epsilon)
+		res = minimize(self.potential, x0, method='nelder-mead',options={'xtol': 1e-10, 'disp': True})
+		return res.x
 
 
-print("couplings")
-print(couplings)
+	def hessian(self, x):
 
-x = np.linspace(1,100,100)
+		A = np.zeros([2,2])
+		A[0,0] = self.sphere_1.charge()*self.trap1.d2xfield(x[[0]]) + self.sphere_1.charge()*self.sphere_2.d2xfield(x[[0]],x[[1]]) + self.sphere_2.charge()*self.sphere_1.d2xfield(x[[0]],x[[1]])
+		A[1,1] = self.sphere_1.charge()*self.trap1.d2xfield(x[[1]]) + self.sphere_1.charge()*self.sphere_2.d2xfield(x[[1]],x[[0]]) + self.sphere_2.charge()*self.sphere_1.d2xfield(x[[1]],x[[0]])
+		A[0,1] = self.sphere_1.charge()*self.sphere_2.dydxfield(x[[0]],x[[1]]) + self.sphere_2.charge()*self.sphere_1.dydxfield(x[[0]],x[[1]])
+		A[1,0] = self.sphere_1.charge()*self.sphere_2.dydxfield(x[[1]],x[[0]]) + self.sphere_2.charge()*self.sphere_1.dydxfield(x[[1]],x[[0]])
+		#change outputs to matrices??
+		return A
+
+
+	def S_matrix(self):
+
+		#calculate 
+		A = self.hessian( self.equilibrium() )
+		eig_vals = np.linalg.eig(A)[0]
+		S = np.linalg.eig(A)[1] 
+		return S
+
+
+	def J(self):
+
+		#calculate couplings 
+		omegaT_vec = np.transpose( [np.array( self.omegaT()) ])
+		dxomega_1 = self.B_field.dxomega()
+		dxomega_vec = np.array([[dxomega_1],[dxomega_1]])
+		deltaZ = self.delta_z()
+		dxomega_mat = np.concatenate( (dxomega_vec, dxomega_vec), axis=1)
+		deltaZ_mat = np.concatenate( (np.transpose(deltaZ), np.transpose(deltaZ)), axis=0)
+		omegaT_mat = np.concatenate( (np.transpose(omegaT_vec), np.transpose(omegaT_vec)), axis=0)
+		S_matrix = self.S_matrix()
+		epsilon = S_matrix*deltaZ_mat*dxomega_mat/omegaT_mat
+		couplings = np.asmatrix( np.transpose(epsilon) ) * np.asmatrix( epsilon*np.transpose(omegaT_mat))
+		return couplings
+
+
+# function to make a system
+def make_sytem(m1,m2,r1,r2,q1,q2,B,V,r):
+
+	sphere_1 = Nanosphere(q1, r1, m1)
+	sphere_2 = Nanosphere(q2, r2, m2)
+	trap_1 = Harmonic_Paul_trap(V, r)
+	B_field_1 = B_field(B)
+
+	system_1 = System(trap_1,sphere_1,sphere_2,B_field_1)
+
+	return system_1
+
+
+
+
+
+#plotting over single variables
+range_x = np.linspace(0,10,100)
+y = np.zeros(100)
+r_calc_1 = np.zeros(100)
+r_calc_2 = np.zeros(100)
+
+
+counter = 0
+for value in range_x:
+
+	system_1 = make_sytem( 171*1.6e-27, 171*1.67e-27, 1e-8, 1e-8, 1.0, .0, value, -0.5 ,0.001 )
+	y[counter] = system_1.J().item((0,1))
+	r_calc_1[counter] = system_1.equilibrium()[[0]]
+	r_calc_2[counter] = system_1.equilibrium()[[1]]
+
+	counter = counter +1
+print(y)
+
+plt.plot(range_x,y)
+#plt.plot(range_x, r_calc_1)
+#plt.plot(range_x, r_calc_2)
+plt.show()
